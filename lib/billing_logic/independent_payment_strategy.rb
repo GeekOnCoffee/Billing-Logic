e09ccfdf7fcee54ac4748d657d377c6028b1cf73
Diego Scataglini <diego@junivi.com>
2012-03-16 17:21:03 -0400

    adding initial implementation of independant payment strategy

---
+++ lib/billing_logic/independent_payment_strategy.rb
@@ -0,0 +1,113 @@
+module BillingLogic
+  class IndependentPaymentStrategy
+    attr_accessor :desired_state, :current_state, :payment_command_builder_class
+    def initialize(opts = {})
+      @current_state = opts.delete(:current_state) || []
+      @desired_state = opts.delete(:desired_state) || []
+      @command_list = []
+      @payment_command_builder_class = opts.delete(:payment_command_builder_class) || PaymentCommandBuilder
+    end
+
+    def desired_state=(value)
+      @desired_state = value
+    end
+
+    def command_list
+      calculate_list
+      @command_list.flatten
+    end
+
+    def calculate_list
+      reset_command_list
+      add_commands_for_products_to_be_added
+      add_commands_for_products_to_be_removed
+    end
+
+    def add_commands_for_products_to_be_added
+      unless products_to_be_added.empty?
+        products_to_be_added.each do |group_of_product, date|
+          @command_list << create_recurring_payment_command(group_of_product, date)
+        end
+      end
+    end
+
+    # Question:
+    # Should the method return a data structure or an object?
+    def products_to_be_added
+      group_by_date(desired_state - current_active_or_pending_products)
+    end
+
+    # this doesn't feel like it should be here
+    def group_by_date(new_products)
+      group = {}
+      new_products.each do |product|
+        if inactive_products.include?(product)
+          next_payment_dates = profiles_by_status(false).select { |profile| profile.products.include?(product) }.map { |profile| profile.next_payment_date }
+          next_payment_date = next_payment_dates.sort.first
+          group[next_payment_date] ||= []
+          group[next_payment_date] << product
+        else
+          group[today] ||= []
+          group[today] << product
+        end
+      end
+      group.map { |k, v| group.assoc(k).reverse } 
+    end
+
+    # for easy stubbing/subclassing/replacement
+    def today
+      Date.today
+    end
+
+    def products_to_be_removed
+      current_active_or_pending_products - desired_state
+    end
+
+    def current_products(opts = {})
+      profiles_by_status(opts[:active]).map { |profile| profile.products }.flatten
+    end
+
+
+    def profiles_by_status(active_or_pending = nil)
+      current_state.reject { |profile| !profile.active_or_pending? == active_or_pending}
+    end
+
+    def inactive_products
+      current_products(active: false)
+    end
+
+    def current_active_or_pending_products
+      current_products(active: true)
+    end
+
+    # this should be part of a separate strategy object
+    def add_commands_for_products_to_be_removed
+      current_state.each do |profile|
+        remaining_products = profile.products.reject { |product| products_to_be_removed.include?(product) }
+        if remaining_products.empty? # all products in payment profile needs to be removed
+          @command_list << cancel_recurring_payment_command(profile.id)
+        elsif remaining_products.size == profile.products.size # nothing has changed
+          # do nothing
+        else  # only some products are being removed and the profile needs to be updated
+          @command_list << cancel_recurring_payment_command(profile.id)
+          @command_list << create_recurring_payment_command(remaining_products, profile.next_payment_date)
+        end
+      end
+    end
+
+    # these messages seems like they should be pluggable
+    def cancel_recurring_payment_command(profile_id)
+      payment_command_builder_class.cancel_recurring_payment_commands(profile_id)
+    end
+
+    def create_recurring_payment_command(products, next_payment_date = Date.today)
+      payment_command_builder_class.create_recurring_payment_commands(products, next_payment_date)
+    end
+
+    def reset_command_list
+      @command_list.clear
+    end
+
+  end
+end
+
---
+++ spec/billing_info/indipendent_payment_strategy_spec.rb
@@ -0,0 +1,205 @@
+require 'spec_helper'
+
+module BillingLogic
+  describe IndependentPaymentStrategy do
+    let(:monthly_cycle) do
+      BillingCycle.new(period: :month, 
+                       frequency: 1,
+                       anniversary: Date.today - 7)
+    end
+
+    let(:yearly_cycle) do
+      BillingCycle.new(period: :year, 
+                       frequency: 1,
+                       anniversary: Date.today - 7)
+    end
+
+    let(:product_a) { mock('Product A', name: 'A', price: 10, billing_cycle: monthly_cycle) }
+    let(:product_b) { mock('Product B', name: 'B', price: 20, billing_cycle: monthly_cycle) }
+    let(:product_c) { mock('Product C', name: 'C', price: 30, billing_cycle: monthly_cycle) }
+    let(:product_d) { mock('Product D', name: 'D', price: 40, billing_cycle: monthly_cycle) }
+    let(:strategy) { IndependentPaymentStrategy.new }
+
+
+    let(:profile_a) do
+      mock('Profile A', 
+            products: [product_a, product_b],
+            price: 30,
+            id: 'i-1',
+            next_payment_date: monthly_cycle.next_payment_date,
+            active_or_pending?: true,
+           )
+    end
+    let(:canceled_profile_d) do
+      mock('Profile D', 
+            products: [product_d],
+            price: 40,
+            id: 'i-4',
+            next_payment_date: monthly_cycle.next_payment_date,
+            active_or_pending?: false,
+           )
+    end
+
+    let(:strategy_with_3_current_products) do
+      IndependentPaymentStrategy.new(current_state:
+                               [profile_a,
+                                mock('Profile B', 
+                                    products: [product_c],
+                                    price: 30,
+                                    id: 'i-2',
+                                    active_or_pending?: true,
+                                   )
+                                ]
+                              )
+    end
+
+    describe "#current_products" do
+      it "should return an array" do
+        strategy.current_products.should be_kind_of(Array)
+      end
+
+      it "should return the products in the current state" do
+        strategy_with_3_current_products.current_products.should == [product_a, product_b, product_c]
+      end
+    end
+
+    describe "#products_to_be_added" do
+      it "should not return products that are already in the current state" do
+        strategy.current_state = []
+        strategy.desired_state = []
+        strategy.products_to_be_added.should be_empty
+      end
+
+      it "should return products that are not in the current state" do
+        strategy.current_state = []
+        strategy.desired_state = [:new_product]
+        strategy.products_to_be_added.should == [[[:new_product], Date.today]]
+      end
+    end
+
+
+    context 'with empty current state' do
+      let(:strategy) { IndependentPaymentStrategy.new }
+
+      context 'with an empty desired state' do
+        it 'should return an empty command list' do
+          strategy.command_list.should == []
+          strategy.command_list.should be_empty
+        end
+      end
+
+      context 'with 1 new subscription in the desired state' do
+        before do
+          strategy.desired_state = [product_a]
+        end
+
+        it "should call create_recurring_payment_command with 1 product on the command builder object" do
+          strategy.payment_command_builder_class.should_receive(:create_recurring_payment_commands).with([product_a], Date.today).once
+          strategy.command_list
+        end
+
+        it 'should return 1 command in the command list' do
+          strategy.command_list.size.should == 1
+        end
+
+      end
+
+    end
+
+    context 'with 2 current profile with 3 products' do
+      context "when going towards a desired state of no products" do
+        before do
+          strategy_with_3_current_products.desired_state = []
+        end
+
+        # NOTE: this should be moved to a separate class
+        it 'should know which product should be removed' do
+          strategy_with_3_current_products.products_to_be_removed.should == [product_a, product_b, product_c]
+        end
+        
+        it "should call :cancel_recurring_payment_command twice" do
+          strategy_with_3_current_products.should_receive(:cancel_recurring_payment_command).twice
+          strategy_with_3_current_products.command_list
+        end
+      end
+
+      context "when making removing a partial product from a profile" do
+        before do 
+          strategy_with_3_current_products.desired_state = [product_b, product_c]
+        end
+
+        it "should cancel the profile with the partial match" do
+          strategy_with_3_current_products.should_receive(:cancel_recurring_payment_command).with('i-1').once
+          strategy_with_3_current_products.command_list
+        end
+
+        it "should create a new profile with the remaining product at the end of of the current billing cycle" do
+          strategy_with_3_current_products.should_receive(:create_recurring_payment_command).with([product_b], profile_a.next_payment_date).once
+          strategy_with_3_current_products.command_list
+        end
+      end
+
+      context "with a yearly subscription" do
+        before do 
+          profile_a.stub(:next_payment_date) { Date.today >> 12 }
+          strategy.current_state = [profile_a]
+        end
+
+        it "should add monthly plan at the end of the year when switching to monthly cycle" do
+          product_a.stub(:billing_cycle) { monthly_cycle }
+          strategy.desired_state = [product_a]
+          strategy.should_receive(:create_recurring_payment_command).with([product_a], profile_a.next_payment_date).once
+          strategy.should_receive(:cancel_recurring_payment_command).with(profile_a.id).once
+          strategy.command_list
+        end
+
+        context "that is cancelled and being re-added" do
+          before do
+            profile_a.stub(:active_or_pending?) { false }
+            strategy.desired_state = [product_a]
+          end
+
+          it "shouldn't try to cancel the yearly subscription if already cancelled" do
+            strategy.should_not_receive(:cancel_recurring_payment_command)
+            strategy.command_list
+          end
+
+          it "should re-add the cancelled product at the end of the year" do
+            strategy.should_receive(:create_recurring_payment_command).with([product_a], Date.today >> 12).once
+            strategy.command_list
+          end
+        end
+      end
+
+      context "with one of them cancelled" do
+        before do 
+          strategy.current_state = [canceled_profile_d]
+        end
+        context "when re-adding the cancelled product" do
+          it "should add it to the end of the cancelled period" do
+            strategy.desired_state = [product_d]
+            strategy.should_receive(:create_recurring_payment_command).with([product_d], canceled_profile_d.next_payment_date).once
+            strategy.command_list
+          end
+        end
+
+      end
+
+      context "with one of them cancelled twice" do
+        before do
+          strategy.current_state = [canceled_profile_d, canceled_profile_d.clone]
+        end
+
+        context "when re-adding the cancelled product" do
+          it "should add it to the end of the cancelled period" do
+            strategy.desired_state = [product_d]
+            strategy.should_receive(:create_recurring_payment_command).with([product_d], canceled_profile_d.next_payment_date).once
+            strategy.command_list
+          end
+        end
+
+      end
+
+    end
+  end
+end
