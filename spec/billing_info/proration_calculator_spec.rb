70bef72465b60964987d0a4e1989cba03f53a8b8
Diego Scataglini <diego@junivi.com>
2012-03-16 17:21:27 -0400

    updated specs

--- spec/billing_info/proration_calculator_spec.rb
+++ spec/billing_info/proration_calculator_spec.rb
@@ -1,59 +1,60 @@
 require 'spec_helper'
 
-describe BillingLogic::ProrationCalculator do
-  include BillingLogic
-  context "calculating monthly prorations" do
-    before do
-      @anniversary_date = Date.parse('1/2/2012')
-      @cycle = BillingCycle.new(period: :month, 
-                                frequency: 1, 
-                                anniversary: @anniversary_date)
-      @price = 29.0 
-      @calculator = ProrationCalculator.new(billing_cycle: @cycle, 
-                                            price: @price)
-    end
-    it "should recall price & cycles" do
-      @calculator.price.should == @price
-      @calculator.billing_cycle.should == @cycle
-    end
-
-    context "from a date prior to the anniversary date" do
-      it "should be able to calculate a proration" do
-        @calculator.price = 31 #adjusting for simplicity
-        @calculator.prorate_from(Date.parse('15/1/2012')).should == 17
+module BillingLogic
+  describe ProrationCalculator do
+    context "calculating monthly prorations" do
+      before do
+        @anniversary_date = Date.parse('1/2/2012')
+        @cycle = BillingCycle.new(period: :month, 
+                                  frequency: 1, 
+                                  anniversary: @anniversary_date)
+        @price = 29.0 
+        @calculator = ProrationCalculator.new(billing_cycle: @cycle, 
+                                              price: @price)
       end
-    end
-
-    context "from a date equal to the anniversary date the proration" do
-      it "should be the full price" do
-        @calculator.prorate_from(@anniversary_date).should == @price
-      end
-    end
-
-    context "from a date posterior to the anniversary date the proration" do
-      it "should be calculated just fine" do
-        @calculator.prorate_from(Date.parse('15/2/2012')).should == 14
+      it "should recall price & cycles" do
+        @calculator.price.should == @price
+        @calculator.billing_cycle.should == @cycle
       end
 
-      it "should return the full price if the future date is the next anniversary date" do
-        @calculator.prorate_from(Date.parse('1/3/2012')).should == 29
+      context "from a date prior to the anniversary date" do
+        it "should be able to calculate a proration" do
+          @calculator.price = 31 #adjusting for simplicity
+          @calculator.prorate_from(Date.parse('15/1/2012')).should == 17
+        end
       end
-    end
 
-  end
-  context "calculating a yearly proration" do
-    before do
-      @anniversary_date = Date.parse('1/1/2013')
-      @cycle = BillingCycle.new(period: :year, 
-                                frequency: 1, 
-                                anniversary: @anniversary_date)
-      @price = 36600
-      @calculator = ProrationCalculator.new(billing_cycle: @cycle, 
-                                            price: @price)
+      context "from a date equal to the anniversary date the proration" do
+        it "should be the full price" do
+          @calculator.prorate_from(@anniversary_date).should == @price
+        end
+      end
+
+      context "from a date posterior to the anniversary date the proration" do
+        it "should be calculated just fine" do
+          @calculator.prorate_from(Date.parse('15/2/2012')).should == 14
+        end
+
+        it "should return the full price if the future date is the next anniversary date" do
+          @calculator.prorate_from(Date.parse('1/3/2012')).should == 29
+        end
+      end
+
     end
-    it "should calculate a proration of $350 on the 1/16" do
-      @calculator.date = Date.parse('16/1/2012')
-      @calculator.prorate.should == 35100
+    context "calculating a yearly proration" do
+      before do
+        @anniversary_date = Date.parse('1/1/2013')
+        @cycle = BillingCycle.new(period: :year, 
+                                  frequency: 1, 
+                                  anniversary: @anniversary_date)
+        @price = 36600
+        @calculator = ProrationCalculator.new(billing_cycle: @cycle, 
+                                              price: @price)
+      end
+      it "should calculate a proration of $350 on the 1/16" do
+        @calculator.date = Date.parse('16/1/2012')
+        @calculator.prorate.should == 35100
+      end
     end
   end
 end
